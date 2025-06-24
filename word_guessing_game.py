7697a1238302a91c4ab7496505c3becd4b23a1ed
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-06-24 20:54:48 +0800

    added the function for needed variables

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -1,1 +1,11 @@
 # guessing game
+import random
+
+class WordGuessGame:
+    def __init__(self, name, word_list, turns=12):
+        super().__init__(name)
+        self.word_list = word_list
+        self.word = random.choice(word_list)
+        self.guesses = 0
+        self.turns = turns
+
