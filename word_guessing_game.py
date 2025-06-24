587d2491fd09468c3aa1c480f0129b856110ab63
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-06-24 22:24:28 +0800

    replaced the 0 on the guesses, and also added comments as explanation to whats happening in the code

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -1,12 +1,21 @@
 # guessing game
 import random
 
-class WordGuessGame:
+# Adding the parent class 
+class Game:
+    def __init__(self, name):
+        self.name = name
+
+    def start_game(self):
+        print(f"Good Luck, {self.name}!")
+
+# Adding the child class
+class WordGuessGame(Game):
     def __init__(self, name, word_list, turns=12):
         super().__init__(name)
         self.word_list = word_list
         self.word = random.choice(word_list)
-        self.guesses = 0
+        self.guesses = ''
         self.turns = turns
 
     def display_word(self):
@@ -15,41 +24,38 @@ class WordGuessGame:
 
             for char in self.word:
                 if char in self.guesses:
-                    display += char + ' '
+                    display += char + ' '     # Displays the letter when the user guessed correctly
                 else:
-                    display += '_ '
+                    display += '_ '    # Displays the underscore when user's answer is incorrect 
                     failed += 1
 
             print(display.strip())
             return failed
 
+# Handles the users turns (how many turns left)
     def play_turn(self):
         guess = input("Guess a character: ")
-        self.guesses += guess
+        self.guesses += guess      # Adds count to the guesses the user can have if they answered correctly
 
         if guess not in self.word:
-            self.turns -= 1
+            self.turns -= 1        # Subtracts count from the guesses if the user guessed wrong
             print("Wrong")
             print(f"You have {self.turns} more guesses.")
         else:
             print("Good guess!")
 
-    def check_win(self, failed):
+# Checks if the failed guesses are 0
+    def check_win(self, failed):   
         if failed == 0:
             print("You Win!")
             print(f"The word is: {self.word}")
             return True
         return False
 
+# Checks if all of the guesses are out, then you are also out
     def check_loss(self):
         if self.turns <= 0:
             print("You Lose")
             print(f"The word was: {self.word}")
             return True
         return False
-
-guess_game = WordGuessGame
-guess_game.display_word()
-guess_game.play_turn()
-guess_game.check_win()
-guess_game.check_loss()
